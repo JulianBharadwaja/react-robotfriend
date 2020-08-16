dbc612a84acc9d762248c18d52ba9f74d396efaa
Julian Bharadwaja <julianbharadwaja1234@gmail.com>
2020-08-16 13:34:40 +0630

    Updates

---
+++ precache-manifest.c06b9ade20e955eea7883737b6872829.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5a96f32ceb6eafd8e316deb4cab4a1d",
+    "url": "/robolast/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robolast/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "c6b48508cb003780e0e9",
+    "url": "/robolast/static/css/main.69f11bbb.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robolast/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robolast/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6b48508cb003780e0e9",
+    "url": "/robolast/static/js/main.de182e67.chunk.js"
+  },
+  {
+    "revision": "17087b599d6add102efc",
+    "url": "/robolast/static/js/runtime-main.ed2a8fe3.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robolast/static/media/SEGA.12a3db8b.woff"
+  }
+]);
